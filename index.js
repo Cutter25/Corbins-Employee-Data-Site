374f191c2330c01806125e00a480f68117f4b4b4
Corbin Cutter <Corbin@Lisas-iMac.lan>
2022-12-01 20:05:28 -0500

    trying to get functions to rerun

--- index.js
+++ index.js
@@ -6,139 +6,122 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Employee = require("./lib/employee");
-
-// generate package requirement
-
-// const generateHTML = require("./src/renderHTMLdoc");
+const renderHTMLdoc = require("./src/renderHTMLdoc");
 
 // Empty space variable to store employee data from user input
 
 let employees = [];
 
-// Quesitons for user
+// Quesitons for user (manager first)
 
-// Choose employee
+function managerQuestions() {
+    inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the managers name?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'What is the managers ID?',
+                name: 'ID',
+            },
+            {
+                type: 'input',
+                message: 'What is the managers email?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'What is the managers office number?',
+                name: 'officeNumber',
+            },
+        ])
+    .then (function managerAnswers () {
+        const {name, ID, email, officeNumber} = managerAnswers;
+        const newManager = new Manager (name, ID, email, officeNumber);
+        employees.push(newManager);
+    })
+    .then (function employeeQuestions () {
+    inquirer.prompt([
+            {
+                type:'list',
+                name:'role',
+                choices:['Engineer', 'Intern']
+            },
+            {
+                type: 'input',
+                message: 'What is the employees name?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'What is the employees ID?',
+                name: 'ID',
+            },
+            {
+                type: 'input',
+                message: 'What is the employees email?',
+                name: 'email',
+            },
+            {
+                type:'input',
+                name:'gitHub',
+                when: (input) => input.role === 'Engineer',
+                message:'What is the Github username of the engineer?',
+            },
+            {
+                type:'input',
+                name:'education',
+                when: (input) => input.role === 'Intern',
+                message:'What school did this employee attend?',
+            },
+            {
+                type: 'list',
+                name:'newEmployee',
+                message:'Would you like to add another employee to the team?',
+                choices: ['Yes', 'No'],
+            },
+        ]) 
+        .then (function employeeAnswers() {
+            let {name, ID, email, role, gitHub, education, newEmployee} = employeeAnswers;
+            let Employee;
+            
+            // data will be passed through constructor function
 
-const employeeType = [
-    {
-        type: 'list',
-        message: 'What type of employee are you entering?',
-        choices: ["Intern", "Engineer", "Manager"],
-        name: 'employeeType',
-    },
-];
+            if (role === 'Engineer'){
+                Employee = new Engineer (name, ID, email, gitHub);
+            }
 
-// Quesitons to build employee card
+            if (role === 'Intern'){
+                Employee = new Intern (name, ID, email, education);
+            }
 
-const questions = [
+            if (newEmployee === 'Yes') {  
+             //   employeeQuestions ()
+             console.log("Hello World!")
+            } else {
+                return employees
+            }
 
-        {
-            type: "input",
-            name: "name",
-            message: "What is the team member's name?",
-        },
-        {
-            type: 'input',
-            name: 'employeeID',
-            message: 'Employee ID?',
-        },
-        {
-            type: 'input',
-            name: 'employeeEmail',
-            message: 'Employee email?',
-        },
-        {
-            type: 'input',
-            name: 'employeeOfficeNumber',
-            message: 'Employee office number?',
-        },
-        {
-            type: 'input',
-            name: 'employeeEducation',
-            message: 'What school is this employee attending for coding education?',
-        },
-        {
-            type: 'input',
-            name: 'employeeEmail',
-            message: 'Employee email?',
-        },
-        {
-            type: "list",
-            name: "newEmployee",
-            message: "Do you want to add another employee?",
-            choices: ["Yes", "No"]
-        }
-]
+            employees.push(Employee)
+        })
+    })
+};    
 
-const writeFile = data =>{
-    fs.renderHTMLDoc("./dist/index.html", data, err=>{
+// function to create HTML file
+
+function writeFile(data) {
+    fs.writeFile("./dist/index.html", data, err)
         if(err){
             console.log(err)
+        } else {
+            console.log("Employee data site has been generated!")
         }
-        else{
-            console.log("Your employee data site has been generated successfully!")
-        }
-    })
 };
 
-function newEmployee() {
-    inquirer.prompt(employeeType)
-    .then(typeOfEmployee => {
-        if (typeOfEmployee.employeeType === "Intern") {
-            inquirer.prompt(quesions)
-            .then(internInfo => {
-                const intern = new Intern
-                (
-                    internInfo.name,
-                    internInfo.employeeID,
-                    internInfo.employeeEmail,
-                    internInfo.employeeEducation,
-                );
-                employees.push(Intern);
-                if (internInfo.newEmployee === "Yes") {
-                    newEmployee();
-                } else {
-                    renderHTMLDoc();
-                }
-            });
-        } else if (typeOfEmployee.employeeType === "Engineer") {
-            inquirer.prompt(quesions)
-            .then(engineerInfo => {
-                const engineer = new Engineer
-                (
-                    engineerInfo.name,
-                    engineerInfo.employeeID,
-                    engineerInfo.employeeEmail,
-                    engineerInfo.employeeGitHubUsername,
-                );
-                employees.push(Engineer);
-                if (engineerInfo.newEmployee === "Yes") {
-                    newEmployee();
-                } else {
-                    renderHTMLDoc();
-                };
-            });
-        } else if (typeOfEmployee.employeeType === "Manager") {
-            inquirer.prompt(quesions)
-            .then(managerInfo => {
-                const manageer = new Manager
-                (
-                    managerInfo.name,
-                    managerInfo.employeeID,
-                    managerInfo.employeeEmail,
-                    managerInfo.employeeOfficeNumber,
-                );
-                employees.push(Manager);
-                if (managerInfo.newEmployee === "Yes") {
-                    newEmployee();
-                } else {
-                    renderHTMLDoc();
-                };
-            });
-        };
-    });
-};
 
 // function to be ran when page is invoked by node index.js
 
-newEmployee();
+managerQuestions();
+
